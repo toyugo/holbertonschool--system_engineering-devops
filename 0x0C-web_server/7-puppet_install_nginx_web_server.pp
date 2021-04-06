2fb79bcf477738216fdf2b0b8dc4186dc9d465f9
O <olivier.guyot@holbertonschool.fr>
2021-04-06 22:20:30 +0000

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,6 @@
 file {'Update index':
     ensure 	=> file,
     path	=> '/var/www/html/index.nginx-debian.html',
-    content     => 'Holberton School\n',
+    content     => 'Holberton School',
 }
 
