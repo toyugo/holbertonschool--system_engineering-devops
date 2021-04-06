baf4acb7ab3f503d60e6e273bc8c7ecb6548fc5d
O <olivier.guyot@holbertonschool.fr>
2021-04-06 22:07:07 +0000

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,12 +1,11 @@
 # puppet manifest
-
 file {'Update index':
     ensure 	=> present,
     path	=> '/var/www/html/index.html',
     content     => 'Holberton School\n'
 }
 file_line { 'adjust redirect',
-    path  => '/etc/nginx/sites-enabled/default'
+    path  => '/etc/nginx/sites-enabled/default',
     line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
     match => '# pass .*',
 }
