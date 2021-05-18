e3ec435a061188c938e9c70f7b85b209cc257756
toyugo <toyugo>
2021-05-18 13:28:37 +0000

    exo1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    """ Return the top ten hot title of reddit.
+    """
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
+                       (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    }
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10"
+    resp = requests.get(url.format(subreddit), headers=headers, allow_redirects=False)
+    if resp.status_code == 404:
+        return 0
+    else:
+        json_resp = resp.json()
+        data_posts = resp.json()['data']['children']
+        for data in data_posts:
+            title = data['data']['title']
+            print(title)
+    return ()
